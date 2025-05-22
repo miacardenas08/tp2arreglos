428451571cbc63e416f00dcffb741e0ac6606cd9
alumno <alumno@Instituto.local>
2025-05-22 11:21:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
 //ejercicio 1
 //1
 let frutas = ["frutilla", "banana", "mandarina"];
-console.log(frutas)
+console.log(frutas);
 
 //2
-frutas.push("manzana")
-console.log(frutas[frutas.length-1])
+frutas.push("manzana");
+console.log(frutas[frutas.length-1]);
 
 //3
-frutas.unshift("pera")
-console.log(frutas[0])
+frutas.unshift("pera");
+console.log(frutas[0]);
 
 //4
-frutas.pop("manzana")
-console.log(frutas)
+frutas.pop("manzana");
+console.log(frutas);
+
+//ejercicio 2
+//1
+let nombres = ["Clara", "Victoria", "Mia"];
+console.log(nombres.length);
+
+//2
+nombres.push("Bauti");
+console.log(nombres);
+
+//3
+console.log(nombres.length)
+
+//ejercicio 3
+//1
+let numeros = ["1", "2", "3", "4"]
+for (let numeros = 0; numeros < numeros.length; numeros++){
+    console.log = array[numeros]
+}
+
 
 
 
 /*
-let alumnos = ["Bauti", "Benja", "facu", "maxi"]
-for (let indice = 0; indice < alumnos.length; indice++){
-    listaAlumnos = listaAlumnos   
-}
+for (let numeros = 0; numeros < numeros.length; numeros++){
+    listaAlumnos = listaAlumnos 
 */
